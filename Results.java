fe668cbb89a5e134e1bbda40eb8a62447cd63c28
aravindababu <aravinda003@gmail.com>
2016-09-27 17:52:13 +0530

    Add files via upload

---
+++ Results.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package results;
+
+/**
+ *
+ * @author user2
+ */
+public class Results {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a = 20;
+        int b = 10;
+        
+       int f=a-- - --a;
+       System.out.println("result " +f );
+        
+       int c=a--; 
+       System.out.println("result " +c );
+       
+       int d=a>>2;
+       System.out.println("result " +d );
+       
+       int e=a&b;
+       System.out.println("result " +e );
+       
+       
+       
+         
+    }
+    
+}
